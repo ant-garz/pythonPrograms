5cab33e1f1194287b5feb28e734b83b3840c76cf
Anthony Garza <anthonylgarza@lewisu.edu>
2019-08-27 20:29:21 -0500

    slight update to selenium tutorial - hit google and send string to search, then search

--- selenium_tutorial_file.py
+++ selenium_tutorial_file.py
@@ -2,13 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
+#locate driver file
 driver = webdriver.Chrome(executable_path="C:\dev\webdriverFiles\chromedriver")
-driver.get("http://www.python.org")
 
-element = driver.find_element_by_name("q")
-element.clear
-element.send_keys("pycon")
-element.send_keys(Keys.RETURN)
-time.sleep(8)
+#navigate to google 
+driver.get("https://www.google.com/")
 
+#use css selector to assign google's input text field to searchInput varbiable
+searchInput = driver.find_element_by_css_selector("#tsf div:nth-child(2) div div.RNNXgb div div.a4bIc input")
+
+#send a string to the search bar
+searchInput.send_keys("This search is automated with python & selenium")
+#equivalent to hitting "enter" to start the search
+searchInput.send_keys(Keys.RETURN)
+
+#explicit wait / make driver sleep for specified amount of time
+time.sleep(3)
+
+#close driver / browser. End of this script
 driver.close()
